3eee5f47ceb149827da5688e5ec7955b8975e825
unknown <ismail.ouali@ump.ac.ma>
2025-02-05 09:12:33 +0100

    Register component fixing

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
-
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmationPopupComponent } from '../../shared/confirmation-popup/confirmation-popup.component';
 
 @Component({
   selector: 'app-register',
@@ -12,7 +13,11 @@ import { AuthService } from '../../services/auth.service';
 export class RegisterComponent {
   registerForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private authService: AuthService) {
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private dialog: MatDialog // Inject MatDialog
+  ) {
     this.registerForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
@@ -27,7 +32,14 @@ export class RegisterComponent {
   onSubmit() {
     if (this.registerForm.valid) {
       this.authService.register(this.registerForm.value);
-      console.log("Yser is registred Sucessfully")
+      this.openConfirmationPopup('Registration successful!');
     }
+  }
+
+  openConfirmationPopup(message: string): void {
+    this.dialog.open(ConfirmationPopupComponent, {
+      width: '300px',
+      data: { message },
+    });
   }
 }
