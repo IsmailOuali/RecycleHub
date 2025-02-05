8d1863d1151adaf2611dee590acaef7f26237443
unknown <ismail.ouali@ump.ac.ma>
2025-02-05 10:03:48 +0100

    Adding the request-form

--- src/app/collect/request-form/request-form.component.ts
+++ src/app/collect/request-form/request-form.component.ts
@@ -4,7 +4,6 @@ import { CollectionService } from '../../services/collection.service';
 
 @Component({
   selector: 'app-request-form',
-  standalone: false,
   templateUrl: './request-form.component.html',
   styleUrls: ['./request-form.component.css'],
 })
@@ -14,16 +13,24 @@ export class RequestFormComponent {
   constructor(private fb: FormBuilder, private collectionService: CollectionService) {
     this.requestForm = this.fb.group({
       wasteType: ['', Validators.required],
-      weight: [null, [Validators.required, Validators.min(1000)]],
+      photos: [''], // Optional field
+      weight: [null, [Validators.required, Validators.min(1000)]], // Minimum 1000g
       address: ['', Validators.required],
-      date: ['', Validators.required],
-      notes: [''],
+      dateTime: ['', Validators.required],
+      notes: [''], // Optional field
     });
   }
 
   onSubmit() {
     if (this.requestForm.valid) {
-      this.collectionService.submitRequest(this.requestForm.value);
+      try {
+        this.collectionService.submitRequest(this.requestForm.value);
+        alert('Request submitted successfully!');
+      } catch (error: any) {
+        alert(error.message); // Display validation errors
+      }
+    } else {
+      alert('Please fill out all required fields correctly.');
     }
   }
 }
